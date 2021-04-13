2a36872e1ce3a5a5d54db34189cfed5c7bf63cf3
cyd <yhoad@connect.ust.hk>
2021-04-14 00:28:11 +0800

    enable 'pref' show/hide

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
-
+import React, { useState } from 'react';
 import MaterialUIPickers from './Component/Date';
 import SimpleSelect from './Component/City';
 import Preferences from './Component/Preferences';
+import Attraction from './Component/PlanAttraction';
 
 function App() {
+  const [show, toggleShow] = useState(false);
+
   return (
     <div className="App">
       <div className="App-header">Travel Planner</div>
-      <div className="input main">
+      <div className="input row">
         <SimpleSelect></SimpleSelect>
         <MaterialUIPickers></MaterialUIPickers>
       </div>
-      <div className="input pref">
-        <p>other preferences</p>
-        <Preferences></Preferences>
+      <div className="input column">
+        <button className="button" onClick={() => toggleShow(!show)}>
+          {show? "collapse": "other preferences"}
+        </button>
+        {show && <Preferences></Preferences>}
+      </div>
+      <div className="output row">
+        <Attraction></Attraction>
       </div>
     </div>
   );
